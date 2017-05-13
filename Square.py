621720d27cb19795173e145d1480e6aea5607308
grant100 <gsowards100@gmail.com>
2017-05-12 23:37:05 -0500

    Randomize Square shape point generation

--- Square.py
+++ Square.py
@@ -1,16 +1,30 @@
 from Shapes import Shapes
+from Line import Line
 import math
 class Square(Shapes):
     def __init__(self):
-        self.origin  = [4,4]
-        self.point_1 = [3,4]
-        self.point_2 = [3,5]
-        self.point_3 = [4,5]
+        self.origin  = []
+        self.point_1 = []
+        self.point_2 = []
+        self.point_3 = []
         self.angle   = math.radians(90)
+        self.generate()
         self.points  = [self.origin, self.point_1, self.point_2, self.point_3]
         Shapes.__init__(self,self.origin, self.points,self.angle)
     def rotate(self):
         return self.points
+
+    def generate(self):
+        line         = Line()
+        self.origin  = line.origin
+        self.point_1 = line.point_2
+        if line.direction > 0:
+           #vertical alignment
+           self.point_2 = [self.origin[0]+1 ,self.origin[1]]
+           self.point_3 = [self.point_1[0]+1,self.point_1[1]]
+        else:
+           self.point_2 = [self.origin[0] ,self.origin[1]+1]
+           self.point_3 = [self.point_1[0],self.point_1[1]+1]
 
 
 
