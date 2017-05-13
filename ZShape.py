e4f3053b3b2483f482f0fa8ad2014c0a4cdf6aa6
grant100 <gsowards100@gmail.com>
2017-05-12 22:08:32 -0500

    cshape

--- ZShape.py
+++ ZShape.py
@@ -1,31 +1,47 @@
 from Shapes import Shapes
 import math
-class ZShapeR(Shapes):
+class ZShape(Shapes):
     def __init__(self):
-        self.origin  = [4,4]
-        self.point_1 = [3,4]
-        self.point_2 = [4,5]
-        self.point_3 = [5,5]
+        self.origin    = []
+        self.point_1   = []
+        self.point_2   = []
+        self.point_3   = []
+        self.angle     = math.radians(90)
+        self.generate()
         self.points    = [self.point_1,self.origin,self.point_2,self.point_3]
-        self.angle   = math.radians(90);
         Shapes.__init__(self,self.origin,self.points,self.angle)
-
     def rotate(self):
-        _points = super(ZShapeR,self).rotate();
+        _points = super(ZShape,self).rotate();
         self.angle = self.angle * -1;
         return _points
+
+    def generate(self):
+        line = line();
+        self.origin  = line.origin
+        self.point_1 = line.point_1
+        if line.direction > 0:
+           #vertical alignment
+           
+   
+class ZShapeR(Shapes):
+    def __init__(self):
+        self.type = 0
+#        self.origin  = [4,4]
+#        self.point_1 = [3,4]
+#        self.point_2 = [4,5]
+#        self.point_3 = [5,5]
+#        self.points    = [self.point_1,self.origin,self.point_2,self.point_3]
+#        self.angle   = math.radians(90);
+        ZShape.__init__(self)
+
 
 class ZShapeL(Shapes):
     def __init__(self):
-        self.origin  = [4,4]
-        self.point_1 = [3,4]
-        self.point_2 = [4,3]
-        self.point_3 = [5,3]
-        self.points    = [self.point_1,self.origin,self.point_2,self.point_3]
-        self.angle   = math.radians(90);
-        Shapes.__init__(self,self.origin,self.points,self.angle)
-
-    def rotate(self):
-        _points = super(ZShapeL,self).rotate();
-        self.angle = self.angle * -1;
-        return _points
+        self.type = 1
+#        self.origin  = [4,4]
+#        self.point_1 = [3,4]
+#        self.point_2 = [4,3]
+#        self.point_3 = [5,3]
+#        self.points    = [self.point_1,self.origin,self.point_2,self.point_3]
+#        self.angle   = math.radians(90);
+        ZShape.__init__(self)
