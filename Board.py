117bcf4b569d1f3dbab2616138e1d567fcbe5470
grant100 <gsowards100@gmail.com>
2017-05-19 00:31:46 -0500

    Board identity updates

--- Board.py
+++ Board.py
@@ -1,7 +1,9 @@
+import unicornhat as unicorn
 class Board():
       def __init__(self):
 	  self.dummy=0;
-	  self.board = self.identity();
+	  self.board      = self.identity();
+	  self.boardColor = self.identityColor();
 
 #      def detect(self,shape):
           
@@ -10,7 +12,8 @@ class Board():
           i = 0
 	  points = shape.points
           for j in points:
-	      self.board[points[i][0],points[i][1]] = shape.color
+	      self.board[points[i][0]][points[i][1]]      = 1
+	      self.boardColor[points[i][0]][points[i][1]] = shape.color
 	      i+=1
           
       def identity(self):
@@ -22,15 +25,23 @@ class Board():
 		   [0,0,0,0,0,0,0,0],
 		   [0,0,0,0,0,0,0,0],
 		   [0,0,0,0,0,0,0,0],]
-	
-      def draw(self):
-	  print '-------Board-------'
-	  print self.board
-	  print '-------------------'
 
-#	  i = 0
-#          for j in self.board
-#	      _x = self.board[i][0]
-#	      _y = self.board[i][1]
-#	      if _x != 0 and _y != 0:
-#	      unicorn.set_pixel(_x,_y,self.color[0],self.color[1],self.color[2]);
+      def identityColor(self):
+          return [ [[],[],[],[],[],[],[],[]],
+                   [[],[],[],[],[],[],[],[]],
+		   [[],[],[],[],[],[],[],[]],
+                   [[],[],[],[],[],[],[],[]],
+		   [[],[],[],[],[],[],[],[]],
+                   [[],[],[],[],[],[],[],[]],
+		   [[],[],[],[],[],[],[],[]],
+                   [[],[],[],[],[],[],[],[]]]
+  	
+      def draw(self):
+	  i = 0
+          for j in self.boardColor:
+	      _x = self.board[i][0]
+	      _y = self.board[i][1]
+		
+ 	      if _x != [] and _y != []:
+	         unicorn.set_pixel(_x,_y,255,255,255);
+	      #unicorn.set_pixel(_x,_y,self.color[0],self.color[1],self.color[2]);
